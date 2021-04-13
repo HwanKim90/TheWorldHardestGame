53058518fcae4ca13765b53316b2ead218f84b85
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2021-04-13 17:42:36 +0900

    플레이어 움직임 추가

---
+++ Unity/Assets/Hwan/Scripts/KH_playerMove.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KH_playerMove : MonoBehaviour
+{
+    public float speed = 5;
+
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 dirV = Vector3.up * v;
+        Vector3 dirH = Vector3.right * h;
+
+        Vector3 dir = dirV + dirH;
+
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
